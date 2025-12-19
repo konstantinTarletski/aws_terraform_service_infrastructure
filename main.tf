877d9b957c91e9df1191d11d88798d3d46bdb1cb
Konstantin Tarletski <konsyantin.tarletski@gmail.com>
2025-12-20 00:09:14 +0200

    fixing gitignore

--- main.tf
+++ main.tf
@@ -11,6 +11,11 @@ provider "aws" {
 }
 
 module "dev-network" {
-  source = "git@github.com:konstantinTarletski/aws_terraform_modules.git//network"
-
+  source      = "git@github.com:konstantinTarletski/aws_terraform_modules.git//network"
+  environment = "dev"
+  default_tags = {
+    Manufactor = "terraform",
+    Design     = "tarlekon",
+    Source     = "module"
+  }
 }
