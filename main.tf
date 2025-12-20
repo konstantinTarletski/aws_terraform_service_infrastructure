bdf4b44b044c154af70733c2f9028c1a357c1bd2
Konstantin Tarletski <konsyantin.tarletski@gmail.com>
2025-12-20 20:47:25 +0200

    Moming terraform state to aws S3

--- main.tf
+++ main.tf
@@ -10,6 +10,14 @@ provider "aws" {
 
 }
 
+terraform {
+  backend "s3" {
+    bucket = "tarlekon-self-aws-terraform-service-infrastructure"
+    key = "dev/network/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
 module "dev-network" {
   source      = "git@github.com:konstantinTarletski/aws_terraform_modules.git//network"
   environment = "dev"
