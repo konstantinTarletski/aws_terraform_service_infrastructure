e1bf2959ebe24b4b2e7bd12eedb48a820f1ac34b
Konstantin Tarletski <konsyantin.tarletski@gmail.com>
2026-01-08 16:47:47 +0200

    Everything works !!! need just clean, and make final version

--- dev/game-sys-test-task/alb/main.tf
+++ dev/game-sys-test-task/alb/main.tf
@@ -36,17 +36,25 @@ data "terraform_remote_state" "shared" {
 }
 
 module "dev_alb" {
-  source                        = "git@github.com:konstantinTarletski/aws_terraform_modules.git//alb_and_ssl"
-  default_tags                  = data.terraform_remote_state.globalvars.outputs.default_tags
-  vpc_id                        = data.terraform_remote_state.network.outputs.vpc_id
-  subnets_ids                   = data.terraform_remote_state.network.outputs.public_subnets_ids
-  environment                   = data.terraform_remote_state.globalvars.outputs.environment
-  project_name                  = data.terraform_remote_state.shared.outputs.git_repository_name_game_sys_test_task
-  alb_http_port                      = 80
-  alb_port_mappings             = { "8815" = { host = "game-sys", priority = 10, health_check = "/swagger-ui.html", is_default = true } }
+  source            = "git@github.com:konstantinTarletski/aws_terraform_modules.git//alb_and_ssl"
+  default_tags      = data.terraform_remote_state.globalvars.outputs.default_tags
+  vpc_id            = data.terraform_remote_state.network.outputs.vpc_id
+  subnets_ids       = data.terraform_remote_state.network.outputs.public_subnets_ids
+  environment       = data.terraform_remote_state.globalvars.outputs.environment
+  project_name      = data.terraform_remote_state.shared.outputs.git_repository_name_game_sys_test_task
+  alb_http_port     = 80
+  alb_port_mappings = { "8815" = { host = "game-sys", priority = 10, health_check = "/swagger-ui.html", is_default = true } }
   //alb_sg_ingress_ports_and_sg   = {} -- default
   //alb_sg_ingress_ports_and_cidr = { "80" = ["0.0.0.0/0"], "443" = ["0.0.0.0/0"] } -- default for HTTPS
+
   //alb_sg_egress_ports_and_sg  -- added in "next step", see "dev/ecs_and_roles/" module add_ecs_sg_to_alb_sg
+  //TODO FIXME or, ..... To avoid circular dependencies use this !!!!!
+/*  alb_sg_egress_ports_and_cidr = { "8815" = concat(
+    [data.terraform_remote_state.network.outputs.vpc_cidr_block],
+    data.terraform_remote_state.network.outputs.vpc_secondary_cidr_blocks
+    )
+  }*/
+
   //alb_sg_egress_ports_and_cidr  = {} -- default
   existing_domain_name = "tarlekon.click"
 }
@@ -56,11 +64,11 @@ output "alb_sg_id" {
 }
 
 output "ports_with_target_groups" {
-  value = module.dev_alb.ports_with_target_groups
+  value       = module.dev_alb.ports_with_target_groups
   description = "Example: ['80080' = 'arn:tg-123']"
 }
 
 output "ports_and_tg_arns_map" {
-  value = module.dev_alb.ports_and_tg_arns_map
+  value       = module.dev_alb.ports_and_tg_arns_map
   description = "Example: ['80080' = {tg_arn = 'arn:tg-123'}]"
 }
