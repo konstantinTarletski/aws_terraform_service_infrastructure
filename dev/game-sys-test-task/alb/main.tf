1cb55535f862e882f8a812ef2e6e5738ae2e25a3
Konstantin Tarletski <konsyantin.tarletski@gmail.com>
2026-01-08 09:52:19 +0200

    Arring horrect host for dns

--- dev/game-sys-test-task/alb/main.tf
+++ dev/game-sys-test-task/alb/main.tf
@@ -43,7 +43,7 @@ module "dev_alb" {
   environment                   = data.terraform_remote_state.globalvars.outputs.environment
   project_name                  = data.terraform_remote_state.shared.outputs.git_repository_name_game_sys_test_task
   alb_http_port                      = 80
-  alb_port_mappings             = { "8815" = { host = "/*", priority = 10, health_check = "/swagger-ui.html", is_default = true } }
+  alb_port_mappings             = { "8815" = { host = "game-sys", priority = 10, health_check = "/swagger-ui.html", is_default = true } }
   //alb_sg_ingress_ports_and_sg   = {} -- default
   //alb_sg_ingress_ports_and_cidr = { "80" = ["0.0.0.0/0"], "443" = ["0.0.0.0/0"] } -- default for HTTPS
   //alb_sg_egress_ports_and_sg  -- added in "next step", see "dev/ecs_and_roles/" module add_ecs_sg_to_alb_sg
